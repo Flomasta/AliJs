a6ef65b4ae7ab0331afab3fb47575adaa75c775b
flomasta <zatevi@mail.ru>
2019-02-21 15:47:18 +0300

    99%

--- src/js/script.js
+++ src/js/script.js
@@ -1,111 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-    const cartWrapper = document.querySelector('.cart__wrapper'),
-        cart = document.querySelector('.cart'),
-        close = document.querySelector('.cart__close'),
-        open = document.querySelector('#cart'),
-        goodsBtn = document.querySelectorAll('.goods__btn'),
-        products = document.querySelectorAll('.goods__item'),
-        confirm = document.querySelector('.confirm'),
-        badge = document.querySelector('.nav__badge'),
-        totalCost = document.querySelector('.cart__total > span'),
-        titles = document.querySelectorAll('.goods__title');
-
-    function openCart() {
-        cart.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    }
-
-    function closeCart() {
-        cart.style.display = 'none';
-        document.body.style.overflow = '';
-    }
-
-    open.addEventListener('click', openCart);
-    close.addEventListener('click', closeCart);
-
-    goodsBtn.forEach(function (btn, i) {
-        btn.addEventListener('click', () => {
-            let item = products[i].cloneNode(true),
-                trigger = item.querySelector("button"),
-                removeBtn = document.createElement('div'),
-                empty = cartWrapper.querySelector('.empty');
-            trigger.remove();
-            showConfirm();
-            removeBtn.classList.add('goods__item-remove');
-            removeBtn.innerHTML = '&times';
-            item.appendChild(removeBtn);
-            cartWrapper.appendChild(item);
-            if (empty) {
-                empty.style.display = 'none';
-            } else {
-                empty.style.display = 'block';
-            }
-            calcGoods();
-            calcTotal();
-            removeFromCart();
-        });
-    });
-
-    function sliceTitle() {
-        titles.forEach(function (item) {
-            if (item.textContent.length < 70) {
-                return;
-            } else {
-                const str = item.textContent.slice(0, 71) + '...';
-                item.textContent = str;
-            }
-        })
-    }
-
-    sliceTitle();
-
-    function showConfirm() {
-        confirm.style.display = 'block';
-        let counter = 100;
-        const id = setInterval(frame, 10);
-
-        function frame() {
-            if (counter == 10) {
-                clearInterval(id);
-                confirm.style.display = 'none';
-            } else {
-                counter--;
-                confirm.style.transform = `translateY(-${counter}px`;
-                confirm.style.opacity = '.' + counter;
-            }
-        }
-    }
-
-    function calcGoods() {
-        const items = cartWrapper.querySelectorAll('.goods__item');
-        badge.textContent = items.length;
-    }
-
-    function calcTotal() {
-        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price');
-        let total = 0;
-        prices.forEach(function (item) {
-            total += parseInt(item.textContent);
-        });
-        totalCost.textContent = total;
-    }
-
-    function removeFromCart() {
-        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
-        removeBtn.forEach(function (item) {
-            item.addEventListener('click', () => {
-                item.parentElement.remove();
-                calcGoods();
-                calcTotal();
-            })
-        })
-    }
-
-    const loadContent = () => {
-        fetch(url)
+    const loadContent = async (url, callback) => {
+        await fetch(url)
             .then(response => response.json())
             .then(json => createElement(json.goods));
+
+        callback();
     }
 
     function createElement(arr) {
@@ -128,14 +27,124 @@ window.addEventListener('DOMContentLoaded', () => {
 
     }
 
-    loadContent('js/db.json');
+    loadContent('js/db.json', () => {
+
+
+        const cartWrapper = document.querySelector('.cart__wrapper'),
+            cart = document.querySelector('.cart'),
+            close = document.querySelector('.cart__close'),
+            open = document.querySelector('#cart'),
+            goodsBtn = document.querySelectorAll('.goods__btn'),
+            products = document.querySelectorAll('.goods__item'),
+            confirm = document.querySelector('.confirm'),
+            badge = document.querySelector('.nav__badge'),
+            totalCost = document.querySelector('.cart__total > span'),
+            titles = document.querySelectorAll('.goods__title');
+
+        function openCart() {
+            cart.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }
+
+        function closeCart() {
+            cart.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+
+        open.addEventListener('click', openCart);
+        close.addEventListener('click', closeCart);
+
+        goodsBtn.forEach(function (btn, i) {
+            btn.addEventListener('click', () => {
+                let item = products[i].cloneNode(true),
+                    trigger = item.querySelector("button"),
+                    removeBtn = document.createElement('div'),
+                    empty = cartWrapper.querySelector('.empty');
+                trigger.remove();
+                showConfirm();
+                removeBtn.classList.add('goods__item-remove');
+                removeBtn.innerHTML = '&times';
+                item.appendChild(removeBtn);
+                cartWrapper.appendChild(item);
+                if (empty) {
+                    empty.style.display = 'none';
+                } else {
+                    empty.style.display = 'block';
+                }
+                calcGoods();
+                calcTotal();
+                removeFromCart();
+            });
+        });
+
+        function sliceTitle() {
+            titles.forEach(function (item) {
+                if (item.textContent.length < 70) {
+                    return;
+                } else {
+                    const str = item.textContent.slice(0, 71) + '...';
+                    item.textContent = str;
+                }
+            })
+        }
+
+        sliceTitle();
+
+        function showConfirm() {
+            confirm.style.display = 'block';
+            let counter = 100;
+            const id = setInterval(frame, 10);
+
+            function frame() {
+                if (counter == 10) {
+                    clearInterval(id);
+                    confirm.style.display = 'none';
+                } else {
+                    counter--;
+                    confirm.style.transform = `translateY(-${counter}px`;
+                    confirm.style.opacity = '.' + counter;
+                }
+            }
+        }
+
+        function calcGoods() {
+            const items = cartWrapper.querySelectorAll('.goods__item');
+            badge.textContent = items.length;
+        }
+
+        function calcTotal() {
+            const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price');
+            let total = 0;
+            prices.forEach(function (item) {
+                total += parseInt(item.textContent);
+            });
+            totalCost.textContent = total;
+        }
+
+        function removeFromCart() {
+            const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+            removeBtn.forEach(function (item) {
+                item.addEventListener('click', () => {
+                    item.parentElement.remove();
+                    calcGoods();
+                    calcTotal();
+                })
+            })
+        }
+    });
+})
+
 
 // fetch('https://jsonplaceholder.typicode.com/posts'),
-    //     {
-    //         method: 'POST',
-    //         body: JSON.stringify(example)
-    //     }
-    //     .then(response => response.json())
-    //     .then(json => console.log(json))
+//     {
+//         method: 'POST',
+//         body: JSON.stringify(example)
+//     }
+//     .then(response => response.json())
+//     .then(json => console.log(json))
 
-})
+
+
+
+
+
